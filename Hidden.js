a4620390ab941d489bc709eff662d1b625190f60
RUHAN KHANDAKAR <ruhankhan25@gmail.com>
2019-03-01 13:39:36 +0530

    Update verison

--- Hidden.js
+++ Hidden.js
@@ -1,78 +1,69 @@
-import React, { Component } from 'react';
-import './pp.css';
+import React, { Component } from "react";
 
 class Hidden extends Component {
-   
-        state = {
-            food : [
-            
-          {name:"persi",lastname:"tongra"},
-          {name:"rashant", lastname:"tongr"},
-         
+    state = {
+        text: "",
+        food: [
+            { name: "persi", lastname: "tongra" },
+            { name: "rashant", lastname: "tongr" }
         ],
-    
-            suggest: [],
-        }
-    
 
-
-
-onSearch = (e) => {
-    const value = e.target.value;
-    let suggest = [];
-    if (value.length > 0) {
-        const regex = new RegExp(`^${value}`, 'i');
-        suggest=this.state.food.map(food => food.name)
-             suggest.sort().filter (v => regex.test(v));
-    }
-     //   suggest = this.state.food.sort().filter (v => regex.test(v));
-    //}
-    this.setState(() => ({ suggest }));
-}
-suggestionsSelected (value) {
-    this.setState(()=>({
-        text:value,
         suggest: []
+    };
+
+    onSearch = e => {
+        const value = e.target.value;
+        let suggest = [];
+        if (value.length > 0) {
+            const regex = new RegExp(`^${value}`, "i");
+            suggest = this.state.food
+                .map(food => food.name)
+                .sort()
+                .filter(v => regex.test(v));
+        }
+        //   suggest = this.state.food.sort().filter (v => regex.test(v));
+        //}
+        console.log(suggest);
+        this.setState(() => ({ suggest }));
+    };
+    suggestionsSelected(value) {
+        this.setState({ text: value, suggest: [] });
     }
-    ))
-}
-toggel=(a)=>{
-    const value=a.target.value;
-    if (value==='ss'){
-        return(
+
+    renderSuggest() {
+        const { suggest } = this.state;
+        if (suggest.length === 0) {
+            return null;
+        }
+
+        return (
+            <ul>
+                {suggest.map(food => (
+                    <li
+                        key={food}
+                        onClick={() => this.suggestionsSelected(food)}
+                    >
+                        {food}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+    render() {
+        return (
             <div>
-                <h1>Patato</h1>
+                <input
+                    type="text"
+                    placeholder="search"
+                    onChange={this.onSearch}
+                    value={this.state.text}
+                />
+                <div>
+                    <div>{this.renderSuggest()}</div>
+                </div>
             </div>
         );
     }
-
-}
-renderSuggest() {
-    const { suggest } = this.state;
-    if (suggest.length === 0) {
-        return null;
-    }
-   
-    return (
-        <ul>
-            {suggest.map((food) => <li>{()=>this.suggestionsSelected()} <div className="acb" onClick={this.toggel}>{food}</div></li>)}
-        </ul>
-    );
-}
-render(){
-return (
-    <div>
-       <input type="text" placeholder="search" onChange={this.onSearch} />
-        <div >
-        <div>
-        {this.renderSuggest()}
-        </div>
-        </div>
-    </div>
-    
-)
-}
-
 }
 
 export default Hidden;
